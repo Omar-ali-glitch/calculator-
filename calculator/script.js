972ad4e5fc39eb73dbce31e467eed4145907d64e
👨‍💻 Omar Ali Code <omaralioffical981@gmail.com>
2026-04-09 17:22:30 +0200

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,20 @@
+//calculator programm
+const display = document.getElementById("display");
+function addToDisplay(input) {
+    display.value += input;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+
+function calculate() {
+    try {     
+    display.value = eval(display.value);
+}
+ catch (error) {
+    display.value = "Error";
+}
+}
+
